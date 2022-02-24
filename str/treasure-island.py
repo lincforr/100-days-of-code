c9c5b9873896549cb783b6347e1c1e313c77c0aa
runlevel <84419743+lincforr@users.noreply.github.com>
2022-02-24 15:40:02 +0000

    stated day 3

---
+++ str/treasure-island.py
@@ -0,0 +1,24 @@
+# Choose your own adventure
+# if condition:
+#     do this
+# else:
+#     do this
+
+# This Works \/
+# water_level = 50
+# if water_level >= 80:
+#     print("drain water")
+# else:
+#     print("continue")
+
+
+# NOTE: Please do not enter any thing other than a intager.
+print("Welcoe to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+
