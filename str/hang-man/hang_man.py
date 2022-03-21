c752d3b1dea208ee5df5a5162a489d3e762b017e
runlevel <84419743+lincforr@users.noreply.github.com>
2022-03-21 22:43:22 +0000

    made a simple Hang Man start

--- str/hang-man/hang_man.py
+++ str/hang-man/hang_man.py
@@ -1,1 +1,15 @@
 # Hang Man
+# https://app.diagrams.net/#Hlincforr%2F100-days-of-code%2Fmain%2Fstr%2Fhang-man%2Fhangman.drawio
+import random
+
+word_list = ["ardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+
+guess = input("Guess a letter: ").lower()
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
