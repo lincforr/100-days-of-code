e01893aab8629b1f7c31be881121bba68ac97617
runlevel <84419743+lincforr@users.noreply.github.com>
2022-03-23 15:30:23 +0000

    added a list to show guessed letters

--- str/hang-man/hang_man.py
+++ str/hang-man/hang_man.py
@@ -2,14 +2,27 @@
 # https://app.diagrams.net/#Hlincforr%2F100-days-of-code%2Fmain%2Fstr%2Fhang-man%2Fhangman.drawio
 import random
 
-word_list = ["ardvark", "baboon", "camel"]
-
+# get word
+word_list = ["ardvark", "baboon", "camel", "apple"]
 chosen_word = random.choice(word_list)
 
+# get _ for each letter
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+
+# testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+# guessing code
 guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
+for position in range(word_length):
+    letter = chosen_word[position]
+    # testing code
+    # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
+
+print(display)
