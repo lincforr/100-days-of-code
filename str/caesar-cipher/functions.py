86af7841aeff46892961adaa250c70872ef96dfd
runlevel <84419743+lincforr@users.noreply.github.com>
2022-04-19 16:39:49 +0000

    edited some of the Askey art

--- str/caesar-cipher/functions.py
+++ str/caesar-cipher/functions.py
@@ -31,18 +31,18 @@
 # print(f"you need {paint_cans_needed} cans to cover your wall")
 
 # Functions
-def prime_checker(number):
-    is_prime = True
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-    if is_prime == False:
-        print("It's not a prime number")
-    elif is_prime == True:
-        print("It's a prime number")
-    # is a prime
-# Running code
-n = int(input("Check this number: "))
-prime_checker(number=n)
+# def prime_checker(number):
+#     is_prime = True
+#     for i in range(2, number):
+#         if number % i == 0:
+#             is_prime = False
+#     if is_prime == False:
+#         print("It's not a prime number")
+#     elif is_prime == True:
+#         print("It's a prime number")
+#     # is a prime
+# # Running code
+# n = int(input("Check this number: "))
+# prime_checker(number=n)
 
 # EOF
